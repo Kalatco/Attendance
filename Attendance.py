c5bef7f357ed971e068e8cbab0c391b26fcf9c2a
Kalatco <Andrewraftovich@gmail.com>
2019-10-18 20:57:12 -0700

    now modifies the Google Sheet

--- Attendance.py
+++ Attendance.py
@@ -22,13 +22,17 @@ class Attendance:
 	sheet = None
 	data = None
 
-	# JSON data of input
+	# JSON data of input as a Dictionary
 	my_students_dict = None
 
-	# temporary dictionary
+	# Lists containing misc. sheet data.
 	my_students = None
 	all_students = None
 	all_sections = None
+
+	# Dictionary containing temporary student data.
+	section_scores = {}
+
 
 	def __init__(self):
 
@@ -52,28 +56,89 @@ class Attendance:
 		self.sheet = self.client.open(sheet_name).sheet1
 		self.data = self.sheet.get_all_records()
 
-		# students in your section
+		# students in your section, excluding row/col describers.
 		self.my_students = self.my_students_dict['students']
-		self.all_students = self.sheet.col_values(1)
-		self.all_sections = self.sheet.row_values(1)
+		self.all_students = self.sheet.col_values(1)[1:]
+		self.all_sections = self.sheet.row_values(1)[1:]
 
 
 	# return a list of a user saved sub-set of users.
-	def getStudents(self):
+	def getMyStudents(self):
 		return self.my_students
+
 
 	# return a list of all updated sections
 	def getSections(self):
 		return(self.all_sections)
+
 
 	# return a list of all updated students
 	def getAllStudents(self):
 		return(self.all_students)
 
 
+	# adds a student to the sub-set that already exists in the Sheets Data.
+	def addStudent(self, student):
+		for definedStudent in self.all_students:
+			if(student == definedStudent):
+				self.my_students.append(student)
+				break
 
+
+	# adds a temporary grade to a student in the sub-set.
+	def addScore(self, date, student, score):
+		if(date not in self.section_scores):
+			self.section_scores[date] = {}
+
+		if(student in self.my_students):
+			self.section_scores[date].update({student: score})
+
+
+	# updates the Sheets data for a specific date and clears temporary data.
+	def submitScores(self, date):
+		if(date in self.section_scores and date in self.all_sections):
+			sectionNumber = 0
+
+			for x in range(len(self.getSections())):
+				if(self.getSections()[x] == date):
+					sectionNumber = x+2
+					break
+
+			if(sectionNumber != 0):
+				for student, score in self.section_scores[date].items():
+					row = 0
+					for x in range(len(self.getAllStudents())):
+						if(student == self.getAllStudents()[x]):
+							row = x+2
+							break
+
+					self.sheet.update_cell(row, sectionNumber, score)
+
+			# dump values of date in dictionary
+			self.section_scores[date] = {}
+
+
+	# returns the temporary section grades
+	def getTempGrades(self):
+		return self.section_scores
+
+# END of Attendance class
+
+
+
+# testing code
 
 p1 = Attendance()
-print(p1.getAllStudents())
+
+print("my students")
+print(p1.getMyStudents())
+
+p1.addScore("Section 3", "Andrew", 2)
+p1.addScore("Section 3", "Sam", 0)
+
+p1.submitScores("Section 3")
+
+
+
 
 
